7e409a7b9c50488cb2af36b76e1d0d8c46d1a646
Vincent Villalta <vincentvillalta@me.com>
2015-04-07 09:17:26 -0600

    fix 9

--- www/src/app.js
+++ www/src/app.js
@@ -128,6 +128,9 @@ angular.module('DicormoApp', ['ionic', 'ngResource', 'ngMessages', 'ngRoute', 'a
     $urlRouterProvider.otherwise('/home/slide');
 
     $httpProvider.defaults.useXDomain = true;
-    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    $httpProvider.defaults.withCredentials = true;
+    delete $httpProvider.defaults.headers.common["X-Requested-With"];
+    $httpProvider.defaults.headers.common["Accept"] = "application/json";
+    $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
 
   }]);
